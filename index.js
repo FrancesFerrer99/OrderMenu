bcb5b9ed023d7e47375bfb2f1d994b1bf756e53c
Frances <frances.ferrer1999@gmail.com>
2022-10-11 15:23:52 +0200

    Final

--- index.js
+++ index.js
@@ -2,19 +2,10 @@ import { menuArray } from "./data.js"
 
 const content = document.getElementById('content')
 const orderDetailsEl = document.getElementById('order-details')
-let order = initializeOrder()
+const modalEl = document.getElementById('modal-checkout')
+const starsReviewEl = document.getElementsByClassName('fa-star')
 
-function initializeOrder(){
-    let newOrder = menuArray.map( item => (
-        {
-            name: item.name,
-            id: item.id,
-            price: item.price,
-            orders: 0
-        }
-    ))
-    return newOrder
-}
+let order = initializeOrder()
 
 document.addEventListener('click', e => {
     if(e.target.dataset.add)
@@ -23,8 +14,18 @@ document.addEventListener('click', e => {
         removeFromCart(e.target.dataset.remove)
     else if(e.target.id === "order-btn")
         handleOrderBtnClick()
+    else if(e.target.id === "close-modal")
+        handleCloseModal()
+    else if(e.target.dataset.index)
+        handleReview(e.target.dataset.index)
 })
 
+document.addEventListener('submit', e => {
+    e.preventDefault()
+    handleFinalCheckout()
+})
+
+//event handlers
 function addToCart(id){
     let targetItem = getItem(id)
     if(targetItem.orders === 0)
@@ -41,6 +42,93 @@ function removeFromCart(id){
     renderSum()
 }
 
+function handleOrderBtnClick(){
+    let price = calculatePrice()
+    if(!price){
+        modalEl.innerHTML = `
+            <h2>No item in cart</h2>
+            <button id="close-modal">Return to order</button>
+        `
+    }
+    else{
+        document.getElementById('order-btn').disabled = true
+        modalEl.innerHTML = `
+            <form>
+                <h2>Enter card details</h2>
+                <label class="modal-label default-border-radius">Enter your name</label><br>
+                <input type="text" placeholder="Enter your name" id="name" required /><br>
+                <label class="modal-label default-border-radius">Enter card number</label><br>
+                <input type="tel" placeholder="Enter card number" id="card" required /><br>
+                <label class="modal-label default-border-radius">CVV</label><br>
+                <input type="text" placeholder="cvv" id="cvv"required /><br>
+                <button type="submit" class="order-btn default-border-radius" id="finish">Pay</button>
+            </form>
+            `
+    }
+    modalEl.classList.remove('hidden')
+}
+
+function handleFinalCheckout(){
+    setTimeout(()=>{
+        modalEl.innerHTML = `
+        <div class="center-text">
+            <img src="images/loading.svg" class="loading">
+            <p id="upload-text">Please wait...</p>
+        </div>
+        `
+    },1000)
+    setTimeout(()=>{
+        modalEl.innerHTML = `
+            <h2>Leave a review</h2>
+            <div class="review">
+                <i class="fa-regular fa-star fa-xl" data-index="0"></i>
+                <i class="fa-regular fa-star fa-xl" data-index="1"></i>
+                <i class="fa-regular fa-star fa-xl" data-index="2"></i>
+                <i class="fa-regular fa-star fa-xl" data-index="3"></i>
+                <i class="fa-regular fa-star fa-xl" data-index="4"></i>
+            </div>
+        `
+        orderDetailsEl.innerHTML = `
+        <div class="default-border-radius default-padding finish-payment">
+            <p>Thanks! Your order is on it's way!</p>
+        </div>
+        `
+        document.getElementById('order-btn').disabled = false
+    },3000)
+    order = initializeOrder()
+}
+
+function handleCloseModal(){
+    modalEl.classList.add('hidden')
+}
+
+function handleReview(index){
+    let maxIndex = parseInt(index)+1
+    for(let i = 0; i < maxIndex; i++){
+        starsReviewEl[i].classList.add('reviewed')
+    }
+    setTimeout(()=>{
+        modalEl.innerHTML = `
+        <p>Thank you!</p>
+        `
+    },1500)
+    setTimeout(()=>{
+        handleCloseModal()
+    },2500)
+}
+
+//utility functions
+function initializeOrder(){
+    let newOrder = menuArray.map( item => (
+        {
+            name: item.name,
+            id: item.id,
+            price: item.price,
+            orders: 0
+        }
+    ))
+    return newOrder
+}
 function getItem(id){
     let newItem = order.filter( item => {
         return item.id === parseInt(id)
@@ -48,21 +136,15 @@ function getItem(id){
     return newItem
 }
 
-function handleOrderBtnClick(){
-
-}
-
 function calculatePrice(){
     let price = 0
     for(let item of order){
-        console.log('price: ', item.price, 'orders: ', item.orders)
         price += item.price*item.orders
-        console.log(price)
     }
-    console.log(price)
     return price
 }
 
+//render related functions
 function renderSum(){
     let orderHtml = ''
     let total = calculatePrice()
